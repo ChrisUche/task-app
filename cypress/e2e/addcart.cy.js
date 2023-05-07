7867d476c3991ca55f7a451ddaeac99c1e1d8248
Uche Chris <chrisuche58@yahoo.com>
2023-05-07 23:02:49 +0100

    update

--- cypress/e2e/addcart.cy.js
+++ cypress/e2e/addcart.cy.js
@@ -1,27 +1,3 @@
-// describe('on click add to cart adds an item to cart', () => { 
-//     it('user can add to cart', () => {
-//         //click on store page
-//         cy.visit('./pages/Store');
-//         // click on add to cart button 
-//         // click on + button 
-        
-//     })
-//  });
-
-// describe('The Home Page', () => {
-//     it('successfully loads', () => {
-//       cy.findByRole('textbox', { name: /Enter a Task/i}).type('nsf'); // change URL to match your dev URL
-//     })
-//   });
-
-// describe('Button Test', () => {
-//   it('Clicks the button and performs the desired action', () => {
-//     cy.visit('http://localhost:3000'); // Change URL to match your dev URL
-//     cy.get('#my-button').click(); // Click the button with id "my-button"
-//     cy.get('#my-result').should('have.text', 'Button clicked!'); // Check if the action was performed correctly
-//   })
-// })
-
 
 //This test visits the homepage, finds the input field and submit button using their respective class names, types in a todo item, clicks the submit button, and then verifies that the new todo item is displayed on the page. inputfield.tsx
 
@@ -61,17 +37,29 @@ describe('InputFields component', () => {
       input.type('buy groceries');
       submitButton.click();
 
-    cy.get('.todos__single').first().trigger('mousedown', { which: 1 });
+    
+      
+    //   cy.get('.todos__single').first().trigger('mousedown', { which: 1 });
 
-    cy.get('.todos__single').trigger('mousemove');
+    //   cy.get('.todos__single').trigger('mousemove');
 
-    cy.get('.todos__single').trigger('mouseup', { force: true });
-
-
+    //  cy.get('.todos__single').trigger('mouseup', { force: true });
 
     
 
+ // get the first todo item
+ const draggable = cy.get('.todos__single').first();
 
+ // get the droppable container
+ const droppable = cy.get('.todos.remove');
+
+ // drag the todo item into completed task
+ draggable.trigger('mousedown', { which: 1 });
+ draggable.trigger('mousemove');
+ droppable.trigger('mouseup', { force: true });
+
+ // check if the item is in completed task
+ //cy.get('.todos.remove').contains('buy groceries');
 
 
   });
